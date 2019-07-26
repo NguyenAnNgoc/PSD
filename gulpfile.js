d949b3db0c0a2768bbe5b39392ae9de3f527e8af
Van Thu <nguyenvanthu1905@gmail.com>
2019-07-26 13:59:32 +0700

    something change

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,64 @@
-var gulp        = require('gulp');
+/*
+plugin: 
+	_gulp-deleted
+	_gulp-collate
+	_gulp-changed
+package:
+	_gulp -v.3.
+	_brower-sync
+*/
+// import package
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-
-// Static server
-gulp.task('browserSync',['css'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-	gulp.watch('css/*css',['css']);
+var deleted = require('gulp-deleted');
+var collate = require('gulp-collate');
+var changed = require('gulp-changed');
+// path
+var img_src = 'Src/Images/*', img_dest='Build/Images/';
+var css_src = 'Src/Styles/*css', css_dst = 'Build/Styles/';
+// Write task
+	//Live Reload
+gulp.task('browserSync',['image','styles'],function(){
+	browserSync.init({
+		server: "./"
+	});
+	//Update styles
+	gulp.watch(css_src,['styles']);
+	//Update image
+	gulp.watch(img_src,['image']);
+	//Update HTML
 	gulp.watch('./*html').on('change',browserSync.reload);
+	//handle delete img-event
+	gulp.watch(img_src).on('change',function(event){
+		if(event.type === 'deleted'){
+			browserSync.reload();
+		}
+	});
 });
-
-// or...
-
-/*gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
-});*/
-gulp.task('css',function(){
-	gulp.src(['css/*css'])
-	.pipe(gulp.dest('css-sdt/'))
+	//Reload style
+gulp.task('styles',function(){
+	gulp.src([css_src])
+	.pipe(gulp.dest(css_dst))
 	.pipe(browserSync.reload({
 		stream: true
-	}))
+	}));
+});
+	//Update images
+gulp.task('image',['update-image'],function(){
 	
-})
+	gulp.src(img_src)
+		.pipe(gulp.dest(img_dest))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+	//update delete image
+gulp.task('update-image',function(){
+	gulp.src(img_src)
+		.pipe(collate('Images'))
+		.pipe(deleted(img_dest,"*"))
+		.pipe(changed(img_dest))
+		.pipe(gulp.dest(img_dest));
+});
+	//Default task
 gulp.task('default',['browserSync']);
